3e79d1ae50eba9bc10b4fe9c18bbbeba6995145a
Mahmut Jomaa <MJomaa@users.noreply.github.com>
2024-09-01 01:48:21 +0200

    feat: add new component MultiSelect

---
+++ components/ui/multi-select.tsx
@@ -0,0 +1,133 @@
+import * as React from 'react';
+import { CaretSortIcon } from '@radix-ui/react-icons';
+import { CheckIcon, X } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+
+export type OptionType = {
+  label: string;
+  value: string;
+};
+
+type MultiSelectProps = {
+  options: OptionType[];
+  selected: string[];
+  onChange: React.Dispatch<React.SetStateAction<string[]>>;
+  className?: string;
+  placeholder?: string;
+};
+
+const MultiSelect: React.FC<MultiSelectProps> = ({
+  options,
+  selected,
+  onChange,
+  className,
+  placeholder = 'Select items...',
+  ...props
+}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleUnselect = React.useCallback(
+    (item: string) => {
+      onChange((prev) => prev.filter((i) => i !== item));
+    },
+    [onChange]
+  );
+
+  const handleSelect = React.useCallback(
+    (value: string) => {
+      onChange((prev) =>
+        prev.includes(value)
+          ? prev.filter((item) => item !== value)
+          : [...prev, value]
+      );
+    },
+    [onChange]
+  );
+
+  return (
+    <Popover
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className={cn(
+            'w-full justify-between',
+            selected.length > 0 && 'h-auto',
+            className
+          )}
+          {...props}
+        >
+          <div className="flex flex-wrap gap-1 items-center">
+            {selected.length > 0 ? (
+              selected.map((item) => (
+                <Badge
+                  variant="secondary"
+                  key={item}
+                  className="mr-1 mb-1"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleUnselect(item);
+                  }}
+                >
+                  {options.find((o) => o.value === item)?.label}
+                  <X className="ml-1 h-3 w-3 text-muted-foreground hover:text-foreground" />
+                </Badge>
+              ))
+            ) : (
+              <span className="text-muted-foreground">{placeholder}</span>
+            )}
+          </div>
+          <CaretSortIcon className="h-4 w-4 shrink-0 opacity-50 ml-2" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className="w-full p-0"
+        align="start"
+      >
+        <Command className={className}>
+          <CommandInput placeholder="Search ..." />
+          <CommandEmpty>No item found.</CommandEmpty>
+          <CommandGroup className="max-h-64 overflow-auto">
+            {options.map((option) => (
+              <CommandItem
+                key={option.value}
+                onSelect={() => handleSelect(option.value)}
+              >
+                <CheckIcon
+                  className={cn(
+                    'mr-2 h-4 w-4',
+                    selected.includes(option.value)
+                      ? 'opacity-100'
+                      : 'opacity-0'
+                  )}
+                />
+                {option.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export { MultiSelect };
