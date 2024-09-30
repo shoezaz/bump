f420a5248b342911187cb83f3459ec384399c28d
Mahmut Jomaa <MJomaa@users.noreply.github.com>
2024-10-01 01:45:00 +0200

    chore: update og.jpg and run linters

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { AppInfo } from '@/constants/app-info';
 import { cn } from '@/lib/utils';
 
-// The logo size below is 28px x 28px (w-7 h-7) but in a 36px x 36px (w-9 h-9) container.
+// The logo size below is 28px x 28px but in a 36px x 36px container.
 // Because of the 8px difference the components <Sidebar /> and <Mobilesheet /> have a pl-0.5 (4px left padding) class applied.
 // When you update the logo make sure to eventually adjust the pl-0.5 class in those two components.
 
@@ -24,8 +24,8 @@ export function Logo({
       {...other}
     >
       {!hideSymbol && (
-        <div className="flex items-center justify-center w-9 h-9 p-1">
-          <div className="flex items-center justify-center w-7 h-7 border rounded-md text-primary">
+        <div className="flex size-9 items-center justify-center p-1">
+          <div className="flex size-7 items-center justify-center rounded-md border text-primary">
             <svg
               width="16"
               height="16"
