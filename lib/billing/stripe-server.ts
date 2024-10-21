4297b7293940a6d0d69e58ce537b2b14e28667a2
Mahmut Jomaa <MJomaa@users.noreply.github.com>
2024-10-22 00:31:03 +0200

    fix: downgrade Stripe

--- lib/billing/stripe-server.ts
+++ lib/billing/stripe-server.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe';
 export const stripeServer = Object.freeze(
   new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
     // https://github.com/stripe/stripe-node#configuration
-    apiVersion: '2024-09-30.acacia'
+    apiVersion: '2024-06-20'
   })
 );
