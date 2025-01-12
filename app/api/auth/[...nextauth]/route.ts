bfc342485cbdcdba860f2eabd213de03d7f3f02f
Mahmut Jomaa <MJomaa@users.noreply.github.com>
2025-01-13 01:14:57 +0700

    chore: remove AUTH_TRUST_HOST

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,23 +2,18 @@ import { NextRequest } from 'next/server';
 
 import { handlers } from '@/lib/auth';
 
-const reqWithTrustedOrigin = (req: NextRequest): NextRequest => {
-  if (process.env.AUTH_TRUST_HOST !== 'true') return req;
-  const proto = req.headers.get('x-forwarded-proto');
-  const host = req.headers.get('x-forwarded-host');
-  if (!proto || !host) {
-    console.warn('Missing x-forwarded-proto or x-forwarded-host headers.');
-    return req;
-  }
-  const envOrigin = `${proto}://${host}`;
+const reqWithOrigin = (req: NextRequest): NextRequest => {
   const { href, origin } = req.nextUrl;
-  return new NextRequest(href.replace(origin, envOrigin), req);
+  return new NextRequest(
+    href.replace(origin, new URL(process.env.NEXT_PUBLIC_BASE_URL as string).origin),
+    req
+  );
 };
 
 export const GET = (req: NextRequest) => {
-  return handlers.GET(reqWithTrustedOrigin(req));
+  return handlers.GET(reqWithOrigin(req));
 };
 
 export const POST = (req: NextRequest) => {
-  return handlers.POST(reqWithTrustedOrigin(req));
+  return handlers.POST(reqWithOrigin(req));
 };
