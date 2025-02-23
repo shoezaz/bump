2382962ee765b36e317c0c39608e652f8b020f07
Mahmut Jomaa <MJomaa@users.noreply.github.com>
2025-02-23 17:04:17 +0100

    style: add transition for max-width container

--- apps/dashboard/app/organizations/[slug]/(organization)/layout.tsx
+++ apps/dashboard/app/organizations/[slug]/(organization)/layout.tsx
@@ -43,7 +43,7 @@ export default async function OrganizationLayout(
         {/* Set max-width so full-width tables can overflow horizontally correctly */}
         <SidebarInset
           id="skip"
-          className="size-full lg:peer-data-[state=collapsed]:max-w-[calc(100svw-var(--sidebar-width-icon))] lg:peer-data-[state=expanded]:max-w-[calc(100svw-var(--sidebar-width))]"
+          className="size-full lg:[transition:max-width_0.2s_linear] lg:peer-data-[state=collapsed]:max-w-[calc(100svw-var(--sidebar-width-icon))] lg:peer-data-[state=expanded]:max-w-[calc(100svw-var(--sidebar-width))]"
         >
           {props.children}
         </SidebarInset>
