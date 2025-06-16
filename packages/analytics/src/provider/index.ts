c5c67cd235a7151fdaff4a4d6dc2695e8ed6b375
Mahmut Jomaa <MJomaa@users.noreply.github.com>
2025-06-17 01:10:31 +0200

    fix: import path

--- packages/analytics/src/provider/index.ts
+++ packages/analytics/src/provider/index.ts
@@ -1,5 +1,4 @@
 export { default as AnalyticsProvider } from './console';
 // export { default as AnalyticsProvider } from './google-analytics';
-// export { default as AnalyticsProvider } from './posthog/client';
-// export { default as AnalyticsServerProvider } from './posthog/server';
+// export { default as AnalyticsProvider } from './posthog';
 // export { default as AnalyticsProvider } from './umami';
